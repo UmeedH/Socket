99ed6660bda9ed0385652013a6119aa7276d5d60
UmeedH <umeedhaiderzaidi@gmail.com>
2016-05-19 21:18:01 -0700

    Update client.py

--- client.py
+++ client.py
@@ -4,8 +4,8 @@ import sys
 from thread import *
 s = socket.socket()         
 host = socket.gethostname()
-port = 5182                # Reserve a port for your service.
-name = raw_input("Name:")
+port = 5188               # Reserve a port for your service.
+msg = raw_input("Enter your message and press enter:")
 s.connect((host, port))
 print s.recv(1024)
 s.close                     # Close the socket when done
@@ -17,6 +17,6 @@ def recieve():
 start_new_thread(recieve,())
 while 1:
 	reply = raw_input()
-	s.send(name+' : '+reply)
+	s.send(msg+' : '+reply)
 	if not reply:
 		break
